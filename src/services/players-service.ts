a40540e9fd2de8d1ac2cb5635e2287653c06fc6d
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-11-13 08:03:31 -0300

    Criar camada Repository

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,9 +1,8 @@
+import * as PlayerRepository from "../repositories/players-repository";
 import { noContent, ok } from "../utils/http-helper";
 
 export const getPlayerService = async () => {
-  const data = {
-    player: "Ronaldo22",
-  };
+  const data = await PlayerRepository.findAllPlayers();
 
   let response = null;
 
